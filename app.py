f6a57c97fa702a2aaa12d22e4ab175df8f0dfd8f
Snedit <sohamde2004@gmail.com>
2023-11-09 23:23:00 +0530

    main files done

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/", methods = ['GET'])
+def input():
+    up_files = list(request.form.get("up-files"))
+    print(up_files)
+    #return "Hello World!"
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ generate.py
@@ -0,0 +1,24 @@
+from reportlab.pdfgen import canvas
+from PIL import Image
+# yayyy this works! 
+# big thanks to chat GPT!
+def convert_jpgs_to_pdf(input_files, output_file):
+    pdf = canvas.Canvas(output_file)
+
+    # Set the page size based on the first image
+    first_img = Image.open(input_files[0])
+    width, height = first_img.size
+    pdf.setPageSize((width, height))
+
+    for input_file in input_files:
+        img = Image.open(input_file)
+        pdf.drawInlineImage(img, 0, 0, width, height)
+        pdf.showPage()  # Start a new page for each image
+
+    pdf.save()
+
+
+input_files = ['soh.jpg', 'args.jpg']
+output_file = 'output.pdf'
+
+convert_jpgs_to_pdf(input_files, output_file)
